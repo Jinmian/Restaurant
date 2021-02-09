36c9faae3b546aca7ad52a0400d5adf7e8469792
Jinmian Zheng <jzhen@natsystem.fr>
2021-02-09 17:59:37 +0100

    Correction responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  afficheBar() {
+    console.log("alert");
+    let x = (<HTMLElement>document.getElementById("myTopnav"))
+    /*let x = document.getElementById("myTopnav") as HTMLElement;*/
+    console.log(x.className);
+
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
   }
 
 }
