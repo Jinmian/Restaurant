e91dba6868c94bb573ee48fefb68c0b879bc6573
Jinmian Zheng <jzhen@natsystem.fr>
2021-02-10 17:55:22 +0100

    Ajout du fichier JSON des menus + traitement

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -1,4 +1,21 @@
+
 import { Component, OnInit } from '@angular/core';
+import studentsData from '../../assets/data/students.json';
+import menuData from '../../assets/data/Menutest.json';
+
+interface Student {
+  id: Number;
+  name: String;
+  email: String;
+  gender: String;
+}
+
+interface Menu {
+  id: Number;
+  name: String;
+  description: String;
+  price: String;
+}
 
 @Component({
   selector: 'app-presentation',
@@ -6,6 +23,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./presentation.component.css']
 })
 export class PresentationComponent implements OnInit {
+  students: Student[] = studentsData;
+  menus: Menu[] = menuData;
 
   constructor() { }
 
